21e59ddcbc079e2afceb0b8f52732a26702f9c48
Fenix <arhange1l@yandex.ru>
2021-03-03 02:57:45 +0300

    Add project files.

---
+++ 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {//Написать метод GetFullName(string firstName, string lastName, string patronymic), принимающий на вход ФИО 
+        //в разных аргументах и возвращающий объединённую строку с ФИО. Используя метод, написать программу, выводящую в консоль 3–4 разных ФИО.
+        static string GetFullName(string firstName, string lastName, string patronymic)
+        {
+            return $"{firstName} {lastName} {patronymic}";
+        }
+        static void Main(string[] args)
+        {          
+            Console.WriteLine(GetFullName("Гена", "Булгаков", "Чебурашкович"));
+            Console.WriteLine(GetFullName("Виталий", "Назаров", "Васильевич"));
+            Console.WriteLine(GetFullName("Рон", "Пон", "Кон"));
+        }
+    }
+}
---
+++ 2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {//Написать программу, принимающую на вход строку — набор чисел, разделенных пробелом, и возвращающую 
+        //число — сумму всех чисел в строке. Ввести данные с клавиатуры и вывести результат на экран.
+        static bool CheckInt(ref string charToString, ref int sum)
+        {
+            if (int.TryParse(charToString, out int paarse))
+            { 
+                sum += paarse;
+                charToString = "";
+                return true;
+            }
+            else
+            {
+                return false;
+            }         
+        }
+
+
+        static void Sum()
+        {
+            bool check = true;
+            string charToString = "";
+            int sum = 0;
+
+            string userValue = Console.ReadLine();
+            for (int i = 0; i < userValue.Length; i++)
+            {
+                if (userValue.Length - 1 == i && userValue[i] != ' ')
+                {
+                    charToString += userValue[i];
+                    check = CheckInt(ref charToString, ref sum);
+                }
+                    if (userValue[i] != ' ')
+                        charToString += userValue[i];
+                    else
+                    {
+                       check = CheckInt(ref charToString, ref sum);
+                        while (userValue[i] == ' ')
+                        {
+                            i++;
+                            if (userValue.Length == i)
+                            {
+                                break;
+                            }
+                        }
+                    i--;
+                    }
+            }
+            if(check)
+                Console.WriteLine($"Сумма чисел = {sum}");
+            else
+                Console.WriteLine("Данные ввдены не корректно!");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите последовательность чисел через пробел");
+            Sum();
+        }
+    }
+}
